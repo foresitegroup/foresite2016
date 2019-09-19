91fbec2c6e99201e83676d394d2aad00ce54abb8
foresitegroup <mark@foresitegrp.com>
2019-09-19 16:02:29 -0500

    Add Wikk to projects

---
+++ wikk.php
@@ -0,0 +1,63 @@
+<?php
+$PageTitle = "Wikk";
+$Description = "Website Design";
+$Keywords = "foresite group, foresite group llc, milwaukee web design, milwaukee website design, creative agency, website design, marketing agency, social media agency, social media marketing, brand design, brand development, logo design, collateral design, search engine marketing agency, milwaukee, seo, brand development, creative solutions, design, print, video";
+$CssInc = "projects.css";
+include "header.php";
+?>
+
+<div class="project wikk-header center">
+  <div class="site-width">
+    WIKK
+
+    <div class="task">WEBSITE DESIGN</div>
+
+    <i class="fa fa-play fa-rotate-90" aria-hidden="true"></i>
+  </div>
+</div>
+
+<div class="site-width wikk-text">
+  Since its founding in 1980, Wikk has been a trusted resource to bridge the "accessibility gap" by delivering patented, code compliant solutions with user-friendly designs. Their products are being used in some of the most iconic buildings in North America, including the Empire State Building, World Trade Center Oculus, Los Angeles Stadium, and United States Capitol, to name a few. Foresite worked closely with Wikk to develop a new branding and digital face that truly reflected the innovative standards they set for themselves. By utilizing a clean interface, we brought their forward-thinking designs to the forefront.<br>
+  <br>
+
+  <a href="https://wikk.com" class="button">Launch Site</a>
+</div>
+
+<div class="wikk-laptop">
+  <img src="images/wikk-laptop.png" alt="">
+</div>
+
+<div class="wikk site-width">
+  <div class="wikk-left">
+    <img src="images/wikk-ipads.png" alt=""><br>
+    <br>
+
+    Wikk's old site buried their innovation and design in a cluttered and text heavy layout. We brought their wide-array of design options to the forefront in an easy to navigate catalog (above) with easy to use filter system. Once customers are done perusing their options, we created an easy-to-use RFQ form (right) that quickly navigates users through the Quoting process with without them having to reference back to the catalog of products.
+  </div>
+
+  <div class="wikk-right">
+    <img src="images/wikk-screenshot.jpg" alt="">
+  </div>
+
+  <div style="clear: both;"></div>
+</div>
+
+<div class="wikk-footer"></div>
+
+<div class="project-nav">
+  <div class="site-width">
+    <div class="prev">
+      <a href="molded-dimensions.php"><i class="fa fa-play fa-flip-horizontal" aria-hidden="true"></i> PREVIOUS PROJECT</a>
+    </div>
+    <div class="home"><a href="work.php"><i class="fa fa-th" aria-hidden="true"></i></a></div>
+    <div class="next">
+      <a href="rivcrete.php">NEXT PROJECT <i class="fa fa-play" aria-hidden="true"></i></a>
+    </div>
+  </div>
+</div>
+
+<div class="prefooter">
+  <h3 class="waves-white"><a href="contact.php">LET'S CREATE SOMETHING</a></h3>
+</div>
+
+<?php include "footer.php"; ?>
